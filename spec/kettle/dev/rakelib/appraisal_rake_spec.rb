af3bf1b84b52bb715042fe8aad55512d878af80e
Peter H. Boling <peter.boling@gmail.com>
2026-06-02 21:24:51 -0600

    Revert "🐛 Fix appraisal update lock repair"
    This reverts commit 4fc2db3e7a57c71659cd1113e0faa949db0269fd.

--- spec/kettle/dev/rakelib/appraisal_rake_spec.rb
+++ spec/kettle/dev/rakelib/appraisal_rake_spec.rb
@@ -12,17 +12,6 @@ RSpec.describe "appraisal rake tasks" do # rubocop:disable RSpec/DescribeClass
 
     before do
       allow(Bundler).to receive(:with_unbundled_env).and_yield
-      allow(Gem::Platform).to receive(:local).and_return(Gem::Platform.new("x86_64-linux"))
-      allow(Dir).to receive(:glob).with(File.join("gemfiles", "*.gemfile")).and_return([
-        "gemfiles/unlocked_deps.gemfile",
-        "gemfiles/current.gemfile"
-      ])
-      allow(File).to receive(:file?).and_call_original
-      allow(File).to receive(:file?).with("gemfiles/unlocked_deps.gemfile.lock").and_return(true)
-      allow(File).to receive(:file?).with("gemfiles/current.gemfile.lock").and_return(false)
-      allow(File).to receive(:read).and_call_original
-      allow(File).to receive(:read).with("gemfiles/unlocked_deps.gemfile").and_return("eval_gemfile \"modular/documentation.gemfile\"")
-      allow(File).to receive(:read).with("gemfiles/unlocked_deps.gemfile.lock").and_return("yaml-converter (0.2.0)")
       allow_any_instance_of(Object).to receive(:system) do |_receiver, *args| # rubocop:disable RSpec/AnyInstance
         system_calls << args
         true
@@ -55,16 +44,10 @@ RSpec.describe "appraisal rake tasks" do # rubocop:disable RSpec/DescribeClass
     it "installs the Appraisal root bundle before updating Bundler" do
       invoke
 
-      expect(system_calls.first(3)).to eq([
+      expect(system_calls).to eq([
         [appraisal_env, "bundle", "install"],
         [appraisal_env, "bundle", "update", "--bundler"],
-        [appraisal_env, "bundle", "install"]
-      ])
-      expect(system_calls).to include(
-        [{"BUNDLE_GEMFILE" => "gemfiles/unlocked_deps.gemfile"}, "bundle", "lock", "--add-platform", "x86_64-linux"],
-        [{"BUNDLE_GEMFILE" => "gemfiles/unlocked_deps.gemfile"}, "bundle", "update", "yaml-converter"]
-      )
-      expect(system_calls.last(2)).to eq([
+        [appraisal_env, "bundle", "install"],
         [appraisal_env, "bundle", "exec", "appraisal", "update"],
         ["bundle", "exec", "rake", "rubocop_gradual:autocorrect"]
       ])
