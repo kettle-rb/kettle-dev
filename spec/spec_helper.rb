dc586ef1b128902a7eb91f30bd2a3fedc710d172
Peter H. Boling <peter.boling@gmail.com>
2026-06-20 07:49:28 -0600

    🎨 Template bootstrap by kettle-jem v7.0.0

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,10 @@ require_relative "config/vcr"
 #       The rescue LoadError handles that scenario.
 begin
   require "kettle-soup-cover"
+  if Kettle::Soup::Cover::DO_COV
+    require "simplecov"
+    SimpleCov.start
+  end
   require "simplecov" if Kettle::Soup::Cover::DO_COV # `.simplecov` is run here!
 rescue LoadError => error
   # check the error message and re-raise when unexpected
