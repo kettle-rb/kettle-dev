d8f893a1dd2b28068b32910b5cf2c46a6e07ec85
Peter H. Boling <peter.boling@gmail.com>
2026-07-31 17:01:15 -0600

    🔖 Bump gem version

--- lib/kettle/dev/version.rb
+++ lib/kettle/dev/version.rb
@@ -5,7 +5,7 @@ module Kettle
     # Version namespace for this gem.
     module Version
       # Current gem version.
-      VERSION = "2.5.13"
+      VERSION = "2.5.14"
     end
     # Current gem version exposed at the traditional constant location.
     VERSION = Version::VERSION # Traditional Constant Location
